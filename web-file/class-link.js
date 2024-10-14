527797a8cc7fc2264965a0f6c4a3cc5eca4f5d56
Inivak2023 <149315444+Inivak2023@users.noreply.github.com>
2024-10-14 21:03:34 +0530

    Add files via upload

---
+++ web-file/class-link.js
@@ -0,0 +1,13 @@
+
+//2025
+
+    var BS2025 = "https://www.example.com";
+    var econ2025 = "https://www.acs.com";
+    var acc2025 = "https://www.inivak.com";
+
+
+//2026
+
+    var BS2026 = "";
+    var econ2026 = "";
+    var acc2026 = "";
