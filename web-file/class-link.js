73c0b71c7c2978ae849ff855b9df0fdb1c29903c
Inivak2023 <149315444+Inivak2023@users.noreply.github.com>
2024-10-16 19:40:03 +0530

    Update class-link.js

--- web-file/class-link.js
+++ web-file/class-link.js
@@ -1,15 +1,18 @@
 
+
+
+
 //2025
 
-var bsLink = localStorage.getItem('bsLink') || "http://www.defaultBS.com";
-var econLink = localStorage.getItem('econLink') || "www.defaultEcon.com";
-var accLink = localStorage.getItem('accLink') || "www.defaultACC.com";
-var ictLink = localStorage.getItem('ictLink') || "www.defaultICT.com";
+var bsLink = "http://www.defaultBS.com";
+var econLink ="www.defaultEcon.com";
+var accLink ="www.defaultACC.com";
+var ictLink ="www.defaultICT.com";
 
 
 
 //2026
 
-    var BS2026 = "";
-    var econ2026 = "";
-    var acc2026 = "";
+    var BS2026 = "http://inivak.com";
+    var econ2026 = "http://inivak.com";
+    var acc2026 = "http://acc.com";
