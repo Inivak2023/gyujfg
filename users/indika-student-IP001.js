6e1ea8198951cf43dd77dee70c7d09ebb807def7
Inivak2023 <149315444+Inivak2023@users.noreply.github.com>
2024-11-30 09:26:33 +0530

    Add files via upload

---
+++ users/indika-student-IP001.js
@@ -0,0 +1,42 @@
+
+
+// arosha indumuna 2025 A/L
+    
+       $("#btn").click(function name() {
+            if ($("#username").val()==="123") {
+                if ($("#password").val()==="123") {
+                    $("#txt").text("");
+                    window.location.href = "AL-stu/AL-sample.html";}
+
+                else{
+                    $("#txt").text("Invalid password");
+                    $("#txt").css("color","red");
+                }
+            }else{
+                $("#txt").text("Invalid username");
+                $("#txt").css("color","red");
+            }
+        })
+
+
+// kaninigayansa 2025 A/L
+
+        $("#btn").click(function name() {
+            if ($("#username").val()==="arosha") {
+                if ($("#password").val()==="arosha2006") {
+                    $("#txt").text("");
+                    window.location.href = "/../..";}
+                
+
+                else{
+                    $("#txt").text("Invalid password");
+                    $("#txt").css("color","red");
+                }
+            }else{
+                $("#txt").text("Invalid username");
+                $("#txt").css("color","red");
+            }
+
+            
+
+        })
